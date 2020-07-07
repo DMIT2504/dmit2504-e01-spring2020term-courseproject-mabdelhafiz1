0eb553d5385a8fcb9e62d312fdb0b8b6339321e8
mabdelhafiz1 <54917695+mabdelhafiz1@users.noreply.github.com>
2020-07-06 23:46:00 -0600

    cleared old method

--- Hangman/app/src/main/java/ca/nait/hangman/MainActivity.java
+++ Hangman/app/src/main/java/ca/nait/hangman/MainActivity.java
@@ -26,19 +26,6 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //declare the variables
-    TextView TextWordToBeGuessed;
-    String wordToBeGuessed;
-    String wordDisplayedString;
-    char[] wordDisplayedCharArray;
-    ArrayList<String> myListOfWords;
-    EditText editInput;
-    TextView TextLettersTried;
-    String LettersTried;
-    final String MESSAGE_WITH_LETTERS_TRIED = "Letters tried";
-    TextView TextTriesLeft;
-    String triesLeft;
-    final String WINNING_MESSAGE="You Won!";
-    final String LOSING_MESSAGE="You Lost!";
     Animation  rotateAnimation;
     Animation scaleAnimation;
     Animation scaleAndRotateAnimation;
@@ -72,88 +59,7 @@ public class MainActivity extends AppCompatActivity {
     Button DragonballZButton;
     Button DragonballSuperButton;
     int clickcount=0;
-
-//fix
-    void revealLetterInWord(String letter){
-        int indexOfLetter = currWord.indexOf(letter);
-        //loop if inde
-        while(indexOfLetter >= 0){
-            wordDisplayedCharArray[indexOfLetter] = currWord.charAt(indexOfLetter);
-            indexOfLetter = currWord.indexOf(letter, indexOfLetter + 1);
-
-        }
-        //update string
-        wordDisplayedString = String.valueOf(wordDisplayedCharArray);
-    }
-
-    void displayWordOnScreen(){
-        String formattedString = "";
-        for(char character : wordDisplayedCharArray){
-            formattedString += character + " ";
-        }
-        TextWordToBeGuessed.setText(formattedString);
-
-    }
-    void initializeGame (){
-        //gridview button
-        ltrAdapt= new LetterAdapter(this);
-        letters.setAdapter(ltrAdapt);
-        //hangman invisible
-        for(int p = 0; p < numParts; p++) {
-            bodyParts[p].setVisibility(View.INVISIBLE);
-        }
-        //new method for guess and won/lost
-
-
-
-
-
-
-
-
-
-
-        //old method
-        //1.Word
-        //shuffle array list and get the first element and then remove it
-//        Collections.shuffle(myListOfWords);
-//        wordToBeGuessed = myListOfWords.get(0);
-//        myListOfWords.remove(0);
-//
-//        //initialize char array
-//        wordDisplayedCharArray = wordToBeGuessed.toCharArray();
-//
-//        // add underscores
-//        for(int i = 1; i < wordDisplayedCharArray.length - 1; i++){
-//            wordDisplayedCharArray[i] = '_';
-//
-//        }
-//        //reveal all occurrences of first characters
-//        revealLetterInWord(wordDisplayedCharArray[0]);
-//        //reveal all occurrences of last characters
-//        revealLetterInWord(wordDisplayedCharArray[wordDisplayedCharArray.length - 1]);
-//
-//        //intialize a string from this char array (for search purposes)
-//        wordDisplayedString = String.valueOf(wordDisplayedCharArray);
-
-        //display word
-//        displayWordOnScreen();
-//        //2. Input
-//        //clear input field
-//        editInput.setText("");
-//        // 3. Letters Tried
-//        //Initialize string for letters tried with a space
-//        LettersTried = " ";
-//
-//        //display on screen
-//        TextLettersTried.setText(MESSAGE_WITH_LETTERS_TRIED);
-
-        //4.Tries Left
-        //initialize the string for tries left
-//        triesLeft = " X X X X X";
-//        //triesLeft =
-//        TextTriesLeft.setText(triesLeft);
-    }
+    boolean clicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -168,76 +74,43 @@ public class MainActivity extends AppCompatActivity {
         //new method intiallize
         rand = new Random();
         currWord = "";
-        wordLayout = (LinearLayout)findViewById(R.id.word);
+        wordLayout = findViewById(R.id.word);
         RevealWordsButton = findViewById(R.id.activity_main_reveal_button);
         DragonballZButton = findViewById(R.id.activity_main_DragonballC_button);
         DragonballSuperButton = findViewById(R.id.activity_main_DragonballSuper_Button);
-        //intialize variables
-//        myListOfWords = new ArrayList<String>();
 
-//        TextWordToBeGuessed = findViewById(R.id.activity_main_word_to_be_guessed_textview);
-//        editInput = findViewById(R.id.activity_main_input_editText);
-//        TextLettersTried = findViewById(R.id.activity_main_letters_used);
-//        TextTriesLeft = findViewById(R.id.activity_main_attemptsLeft);
         //animation
         rotateAnimation  = AnimationUtils.loadAnimation(this,R.anim.rotate);
         scaleAnimation = AnimationUtils.loadAnimation(this,R.anim.scale);
         scaleAndRotateAnimation = AnimationUtils.loadAnimation(this,R.anim.scale_and_rotate);
-        scaleAndRotateAnimation.setFillAfter(true);
-//
+        
         //body
         bodyParts = new ImageView[numParts];
-        bodyParts[0] = (ImageView)findViewById(R.id.head);
-        bodyParts[1] = (ImageView)findViewById(R.id.body);
-        bodyParts[2] = (ImageView)findViewById(R.id.arm1);
-        bodyParts[3] = (ImageView)findViewById(R.id.arm2);
-        bodyParts[4] = (ImageView)findViewById(R.id.leg1);
-        bodyParts[5] = (ImageView)findViewById(R.id.leg2);
+        bodyParts[0] = findViewById(R.id.head);
+        bodyParts[1] = findViewById(R.id.body);
+        bodyParts[2] = findViewById(R.id.arm1);
+        bodyParts[3] = findViewById(R.id.arm2);
+        bodyParts[4] = findViewById(R.id.leg1);
+        bodyParts[5] = findViewById(R.id.leg2);
 
         //letters
         letters = findViewById(R.id.letters);
         playGame();
-        //traverse database file and populate array list
-//        InputStream myInputStream = null;
-//        Scanner in = null;
-//        String  aWord = "";
-//        try {
-//            myInputStream = getAssets().open("database_file.txt");
-//            in = new Scanner(myInputStream);
-//            while(in.hasNext()){
-//                aWord = in.next();
-//                myListOfWords.add(aWord);
-//
-//            }
-//        } catch (IOException e) {
-//            Toast.makeText(MainActivity.this, e.getClass().getSimpleName() + ": " + e.getMessage(),
-//            Toast.LENGTH_SHORT).show();
-//        }
-//        finally{
-//            //close scanner
-//            if(in != null)
-//            {
-//                in.close();
-//            }
-            //close InputStream
-//            try {
-//                if(myInputStream != null) {
-//                    myInputStream.close();
-//                }
-//            } catch (IOException e) {
-//                Toast.makeText(MainActivity.this, e.getClass().getSimpleName() + ": " + e.getMessage(),
-//                        Toast.LENGTH_SHORT).show();
-//            }
 
         RevealWordsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO Auto-generated method stub
-                clickcount=clickcount+1;
-                charViews[clickcount].setTextColor(Color.BLACK);
-                if(clickcount >= 6){
+                if(clicked)
+                {
+                    clickcount =0;
+                }
+                clickcount+=1;
+                charViews[clickcount].setTextColor(Color.GREEN);
+                if(clickcount >= 5){
                     RevealWordsButton.setEnabled(false);
                 }
+
                 //start animation
                 v.startAnimation(rotateAnimation);
             }
@@ -247,7 +120,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 playDbzEpisdoes();
-
+                clicked=true;
+                RevealWordsButton.setEnabled(true);
                v.setAnimation(scaleAndRotateAnimation);
             }
         });
@@ -255,37 +129,15 @@ public class MainActivity extends AppCompatActivity {
         DragonballSuperButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                clicked=true;
                 playDbs();
+                RevealWordsButton.setEnabled(true);
                 v.setAnimation(scaleAndRotateAnimation);
             }
         });
         }
 
-         //initializeGame();
 
-        // setup the text changed listner for the edit text
-//        editInput.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//                    // if there is some letter on the input field
-//                if(s.length() != 0){
-//                    chechIfLetterIsInWord(s.charAt(0));
-//                }
-//
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//
-//            }
-//        });
-
-//    }
     private void playGame()
     {
         //play a new game
@@ -392,62 +244,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-//    void chechIfLetterIsInWord(char letter){
-//         // if the letter is found in the word to be guessed
-//        if(wordToBeGuessed.indexOf(letter) >= 0){
-//            // if the letter was Not displayed yet
-//            if(wordDisplayedString.indexOf(letter) < 0){
-//                //animate
-//                TextWordToBeGuessed.startAnimation(scaleAnimation);
-//                //replace the underscores with that letter
-//                revealLetterInWord(letter);
-//                //update the changes on screen
-//                displayWordOnScreen();
-//                //check if the game is won
-//                if(!wordDisplayedString.contains("_")){
-//                    TextTriesLeft.startAnimation(scaleAndRotateAnimation);
-//                    TextTriesLeft.setText(WINNING_MESSAGE);
-//                }
-//            }
-//
-//        }
-//        //otherwise, if the letter was Not found inside the word to be guessed
-//        else {
-//            //decrease the number of tries left, and we'll show it on screen
-//            //decreasesAndDisplayTriesLeft(); //this crashes
-//            //check if the game is lost
-//            if(triesLeft.isEmpty()){
-//                TextTriesLeft.startAnimation(scaleAndRotateAnimation);
-//                TextTriesLeft.setText(LOSING_MESSAGE);
-//                TextWordToBeGuessed.setText(wordToBeGuessed);
-//            }
-//        }
-        //display the letter that was tried
-
-//    }
-    //if you put 2 it crashes
-//    void decreasesAndDisplayTriesLeft() {
-//        //if there are still some tries left
-//        if(!triesLeft.isEmpty()){
-//            // animate scale
-//            TextTriesLeft.startAnimation(scaleAnimation);
-//            //take out the last 2 characters from this string
-//            triesLeft = triesLeft.substring(0, triesLeft.length() - 1);
-//            TextLettersTried.setText(triesLeft);
-//        }
-
-//    }
-
-
-//    public void ResetGame(View view) {
-//        //start animation
-//        view.startAnimation(rotateAnimation);
-//        //setup new game
-//        //initializeGame();
-//        playGame();
-//        editInput.setText("");
-//
-//    }
 
     public void letterPressed(View view) {
         //when player taps button letterpressed recieves a reference
@@ -528,16 +324,6 @@ public class MainActivity extends AppCompatActivity {
         for (int l = 0; l < numLetters; l++) {
             letters.getChildAt(l).setEnabled(false);
         }
-    }
-
-    public void RevealWords(View view) {
-        charViews[0].setTextColor(Color.BLACK);
-        charViews[1].setTextColor(Color.BLACK);
-
-
-        //start animation
-      view.startAnimation(rotateAnimation);
-
     }
 
 }
